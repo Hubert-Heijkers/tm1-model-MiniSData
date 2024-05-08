fe44430751f14d83a5efdf55616854cef83425bf
Hubert Heijkers <hubert@heijkers.com>
2024-05-08 17:51:42 +0200

    Improve model validation script

--- scripts/validate_model.py
+++ scripts/validate_model.py
@@ -1,5 +1,7 @@
 import requests
 import base64
+import json
+import sys
 
 tm1_service_root_url = "http://cwc-tm1-v12-demo.tm1-code.io:4444/tm1/api/v1"
 username = "hubert@tm1-code.io"
@@ -15,7 +17,7 @@ def send_post_request(endpoint, payload):
     }
     response = requests.post(full_url, headers=headers, json=payload)
     if response.text:
-        return response.status_code, response.json()
+        return response.status_code, json.dumps(response.json(), indent=4)
     else:
         return response.status_code, {}
 
@@ -26,7 +28,7 @@ def send_delete_request(endpoint):
     }
     response = requests.delete(full_url, headers=headers)
     if response.text:
-        return response.status_code, response.json()
+        return response.status_code, json.dumps(response.json(), indent=4)
     else:
         return response.status_code, {}
 
@@ -35,11 +37,20 @@ databases_endpoint = "Databases"
 payload = {
     "Name": f"{database_name}"
 }
+
+# Create the database on our target TM1 service instance
+status_code, response = send_post_request(databases_endpoint, payload)
+if status_code != 201:
+    print(f">>> Creation of database {database_name} failed! Status Code: {status_code} Response:\n{response}")
+    sys.exit(1)
+print(f">>> Database {database_name} created successfully! Response:\n{response}")
+
+# Compose the endpoint for the newly created database 
 database_endpoint = f"{databases_endpoint}('{database_name}')"
 
-status_code, response = send_post_request(databases_endpoint, payload)
-print(f"Create database {database_name}:\nStatus Code: {status_code}\nResponse: {response}\n")
-
-if status_code == 201 :
-    status_code, response = send_delete_request(database_endpoint)
-    print(f"Delete database {database_name}:\nStatus Code: {status_code}\nResponse: {response}\n")
+# Last but not least, now that we're done testing, lets cleanup by removing the database
+status_code, response = send_delete_request(database_endpoint)
+if status_code != 204:
+    print(f">>> Deletion of database {database_name} failed! Status Code: {status_code} Response:\n{response}")
+    sys.exit(1)
+print(f">>> Database {database_name} deleted!")
