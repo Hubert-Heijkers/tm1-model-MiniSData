3c7250249bca31d2ddb46361826d313d855d597c
Hubert Heijkers <hubert@heijkers.com>
2024-05-09 12:01:00 +0200

    Added and updated deploy scripts

---
+++ .scripts/delete_model.py
@@ -0,0 +1,20 @@
+import sys
+from common import *
+
+tm1_database_name = "MiniSData"
+tm1_service_root_url = "http://cwc-tm1-v12-demo.tm1-code.io:4444/tm1/api/v1"
+tm1_service_username = "hubert@tm1-code.io"
+tm1_service_password = "apple"
+
+# Setup session and authentication
+session = setup_session(tm1_service_username, tm1_service_password)
+
+databases_endpoint = "Databases"
+database_endpoint = f"{databases_endpoint}('{tm1_database_name}')"
+
+# Remove the database
+status_code, response = send_delete_request(session, tm1_service_root_url, database_endpoint)
+if status_code != 204:
+    log_output(f"Deletion of database {tm1_database_name} failed!", status_code, response)
+    sys.exit(1)
+log_output(f"Database {tm1_database_name} deleted!")
