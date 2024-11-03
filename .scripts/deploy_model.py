79da5ddc90a58f97f27b43402f30ad9b32ee9e6b
Hubert Heijkers <hubert@heijkers.com>
2024-11-03 15:01:01 +0100

    Prep for 7. TM1 Anwendertag GitOps demo
    Switch to using our TM1 v12 local service running on 7-anwendertag-demo.tm1-code.io, the tm1 service instance that we use (7-anwendertag),
    as well as users that are authenticated using our tm1-authenticator-service for the example.com domain.

--- .scripts/deploy_model.py
+++ .scripts/deploy_model.py
@@ -2,8 +2,8 @@ import sys
 from common import *
 
 tm1_database_name = "MiniSData"
-tm1_service_root_url = "http://cwc-tm1-v12-demo.tm1-code.io:4444/tm1/api/v1"
-tm1_service_username = "hubert@tm1-code.io"
+tm1_service_root_url = "http://7-anwendertag-demo.tm1-code.io:4444/7-anwendertag/api/v1"
+tm1_service_username = "github-actions@example.com"
 tm1_service_password = "apple"
 
 # Setup session and authentication
